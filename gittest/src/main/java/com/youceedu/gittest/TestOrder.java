60ce2598d20dcb9a74c3d628b2f407b93ce02eff
sunhongzhen128 <892799436@qq.com>
2020-07-15 18:29:01 +0800

    first commit TestOrder.java

---
+++ gittest/src/main/java/com/youceedu/gittest/TestOrder.java
@@ -0,0 +1,11 @@
+package com.youceedu.gittest;
+
+public class TestOrder {
+
+	public static void main(String[] args) {
+		System.out.println("TestOrder1");
+		System.out.println("TestOrder2");
+
+	}
+
+}
